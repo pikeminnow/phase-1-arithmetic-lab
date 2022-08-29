e5c6a6a0ebdd3405545c358bb7d47642d406a96b
Ashton MacKenzie <pikeminnow@gmail.com>
2022-08-29 13:23:27 -0400

    commit tree a8272bc182fe9596f08f9e1dd211d02ee41acc55

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 28;
+let num4 = 8;
+let mod = num3 % num4;
+
+const max = Math.max(12, 15, 18, 20);
